eaf4a46bb374dc91b5179fe2b18d7312d8dd8dc2
itsjoetree <84997596+itsjoetree@users.noreply.github.com>
2023-10-17 21:07:22 -0400

    optimized for mobile

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,10 +1,10 @@
 import { ComponentChildren } from "preact";
 
-const Main = ({ children }: { children: ComponentChildren }) => (<div className="border border-blue-400 rounded-lg px-4 pb-6 flex flex-col gap-6 w-full max-w-md md:max-w-2xl lg:max-w-5xl max-h-screen overflow-y-auto">
+const Main = ({ children }: { children: ComponentChildren }) => (<main className="border border-blue-400 rounded-lg px-4 pb-6 flex flex-col gap-6 w-full max-w-md md:max-w-2xl lg:max-w-5xl lg:max-h-screen overflow-y-auto">
   <div className="pt-2 pb-2 text-lg flex gap-1 text-blue-400 select-none sticky top-0 w-full bg-black">
     {Array(3).fill(<>&#x2022;</>).map((dot, i) => <span key={i}>{dot}</span>)}
   </div>
   {children}
-</div>);
+</main>);
 
 export default Main;
